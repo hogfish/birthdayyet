fa2395490bacbd2766d9a2db6a8ca113e2bb8fea
Trish Khoo <trishkhoo@gmail.com>
2012-10-18 23:01:18 +0100

    Hacked together a birthday present

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,38 @@
+require 'Date'
+class HomeController < ApplicationController
+  def index
+    @current_date = Time.now
+    do_it
+  end
+
+  def the_time_is_now
+    @current_date = Time.new(2012, 10, 7, 0, 0, 0, 0)
+    do_it
+  end
+
+  def do_it
+    if birthday_today?
+      @answer = "Yes!"
+      @additional = "Happy birthday, Simon! If you're not Simon, and you haven't already got him a present, maybe you should buy something like "
+      @link = "http://www.thefancy.com/things/shuffle?ctid=186895654038015905"
+    else
+      @answer = "No."
+      @additional = "Did you forget Simon's birthday? Don't worry, you still have time to buy him a present. Maybe something like "
+      @link = "http://www.thefancy.com/things/shuffle?ctid=186895654038015905"
+    end
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @countries }
+    end
+  end
+
+  def birthday_today?
+    if @current_date.month == 10 && @current_date.day == 7
+      true
+    else
+      false
+    end
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Birthdayyet::Application.config.secret_token = '69a890f2f3059183808fb27f6a68adf23d73ab87057e3a972dbefd402d7fe6e2c148d995a8901d059a2aca2ab0df45e43f4426817687e57641753268c70327be'
